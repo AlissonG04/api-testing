70e1eb9de5868b7d98832f03e18ad24445939166
Alisson Gabriel <eng.alissongabriel@gmail.com>
2023-10-15 18:53:42 -0300

    feat: routes config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const routes = require("./routes/routes");
+
 const app = express();
 
-const port = PORT.env || 3000;
+app.use(express.json());
+
+const port = 3000;
+
+app.use(routes);
 
 app.listen(port);
